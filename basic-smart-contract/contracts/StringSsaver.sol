1087173282415f96b93f4530995eb465a5d18ab8
celyssa <143862783+celyssa@users.noreply.github.com>
2024-02-17 02:25:05 +0000

    Pending changes exported from your codespace

---
+++ basic-smart-contract/contracts/StringSsaver.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+
+contract StringSsaver {
+      string public data;
+
+          function setData(string memory _data) public payable {
+        data = _data;
+    }
+}
